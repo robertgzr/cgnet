59e8eff3c92a0815f748165d318a1e1f6f9f289e
Robert Günzler <robertguenzler@kinvolk.io>
2017-07-24 18:19:38 +0200

    Install bpf program for every pod we discover
    This introduces a bpf controller that runs the lookup loop for
    every cgroup we attach a program to, reads the map and executes
    handlers that write the tracked data to Prometheus metrics.
    
    The top subcommand uses the same code but implements different handlers.
    
    Closes #4, closes #5, closes #6, closes #9

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ RUN apk update && apk add --no-cache libc6-compat
 COPY cgnet /cgnet
 
 EXPOSE 9101
-ENTRYPOINT ["/cgnet", "serve"]
+ENTRYPOINT ["/cgnet", "export"]
 CMD ["--port", "9101"]
---
+++ bpf/controller.go
@@ -0,0 +1,89 @@
+/*
+Copyright 2017 Kinvolk GmbH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package bpf
+
+import (
+	"context"
+	"log"
+	"time"
+
+	bpf "github.com/iovisor/gobpf/elf"
+)
+
+/*
+#include <linux/bpf.h>
+*/
+import "C"
+
+const (
+	packetsKey uint32 = 0
+	bytesKey   uint32 = 1
+)
+
+type Controller struct {
+	cgroup string
+	module *bpf.Module
+	quit   chan struct{}
+
+	packetsHandler func(uint64) error
+	bytesHandler   func(uint64) error
+}
+
+func (c *Controller) Stop() {
+	c.quit <- struct{}{}
+	if err := c.module.Close(); err != nil {
+		log.Printf("error closing bpf module: %s", err)
+	}
+}
+
+func (c *Controller) SetPacketsHandler(h func(uint64) error) {
+	c.packetsHandler = h
+}
+func (c *Controller) SetBytesHandler(h func(uint64) error) {
+	c.bytesHandler = h
+}
+
+func (c *Controller) Run(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-c.quit:
+			return
+		case <-time.After(1 * time.Second):
+			// TODO this needs to be solved differently
+			// Maybe sending new values on individual channels?
+			packets, err := lookup(c.module, packetsKey)
+			if err != nil {
+				log.Printf("lookup failed: %s", err)
+				continue
+			}
+			if err := c.packetsHandler(packets); err != nil {
+				log.Printf("packetsHandler failed: %s", err)
+			}
+
+			bytes, err := lookup(c.module, bytesKey)
+			if err != nil {
+				log.Printf("lookup failed: %s", err)
+				continue
+			}
+			if err := c.bytesHandler(bytes); err != nil {
+				log.Printf("bytesHandler failed: %s", err)
+			}
+		}
+	}
+}
