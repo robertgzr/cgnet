9d9bbe6a461d6c87883dc1aaed648da9beaf2724
Robert Günzler <robertguenzler@kinvolk.io>
2018-05-08 14:39:22 +1200

    Use k8s.io/api as direct dependency

--- kube/kube.go
+++ kube/kube.go
@@ -20,9 +20,9 @@ import (
 	"context"
 	"time"
 
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 )
